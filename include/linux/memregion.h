9d2bbb7a56d5697e051a0c02ab623b83fc1e8a83
Dan Williams <dan.j.williams@intel.com>
2019-03-29 09:39:07 -0700

    lib/memregion: Uplevel the pmem "region" ida to a global allocator
    In preparation for handling platform differentiated memory types beyond
    persistent memory, uplevel the "region" identifier to a global number
    space. This enables a device-dax instance to be registered to any memory
    type with guaranteed unique names.
    
    Cc: Keith Busch <keith.busch@intel.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

---
+++ include/linux/memregion.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef _MEMREGION_H_
+#define _MEMREGION_H_
+#include <linux/types.h>
+
+int memregion_alloc(gfp_t gfp);
+void memregion_free(int id);
+#endif /* _MEMREGION_H_ */
